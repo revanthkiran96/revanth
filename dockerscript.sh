70bf45b47bdbb624eff1986468ef23ce489fd939
revanthkiran96 <116859708+revanthkiran96@users.noreply.github.com>
2023-02-25 08:07:50 +0530

    Create dockerscript.sh

---
+++ dockerscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo chkconfig docker on
+docker run -rm -itd --name=container1 -p 8000:80 nginx:v3
