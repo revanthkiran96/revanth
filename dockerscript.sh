305f2ee2c5ada5f92447ac6b028a5cb84890401d
revanthkiran96 <116859708+revanthkiran96@users.noreply.github.com>
2023-02-25 08:14:54 +0530

    Update dockerscript.sh

--- dockerscript.sh
+++ dockerscript.sh
@@ -3,4 +3,4 @@ sudo amazon-linux-extras install docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo chkconfig docker on
-docker run -rm -itd --name=container1 -p 8000:80 nginx:v3
+docker run --rm -itd --name=container1 -p 8000:80 nginx:v3
